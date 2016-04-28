750bc3b36e8cfc47d9a84fdeabf73bd7639f7e55
Paul Andre Rada <paulriverarada@gmail.com>
2016-04-27 22:41:04 -0700

    The reader can now chain itself to create read chains
    -The user can now add the string 'read' to any response and pass a new responseList to keep analizing the command with a smaller set of response objects

--- bot_modules/reader.js
+++ bot_modules/reader.js
@@ -5,28 +5,32 @@ var noCommandSet = require('./responses/no-command-set');
 var contextParser = require('./helpers/context-parser');
 var ERROR_LOGS = require('./helpers/error-logs');
 
-module.exports = function(context){
+var reader = function(context){
+    var listObj = {},
+      context = context;
 
-    // Make sure the context argument is safe
-    try{ contextParser(context) }
-    catch(error){ console.log('Reader Error: ', error); }
-    // Make sure the responseList is safe
-    try{ context.bot.responseList }
-    catch(error){ console.log('Reader Error: ', error); }
+    var tryContext = function(context){
+      // Make sure the context argument is safe
+      try{ contextParser(context) }
+      catch(error){ console.log('Reader Error: ', error); }
+      // Make sure the responseList is safe
+      try{ context.bot.responseList }
+      catch(error){ console.log('Reader Error: ', error); }
+    };
 
     var findResponse = function(){
       var list = context.bot.responseList,
           length = list.length,
           message = context.bot.message,
-          ability, commands, notFound;
-      // Go trough the available ability objects
+          responseObj, commands, notFound;
+      // Go trough the available reseponse objects
       // inside the responseList
       for( var i=0; i<length; i++ ){
-        ability = list[i];
-        commands = ability.commands;
+        responseObj = list[i];
+        commands = responseObj.commands;
         // save the users commandNotFound response
-        notFound = ability.name === "commandNotFound"?
-          ability.response : noCommandSet;
+        notFound = responseObj.name === "commandNotFound"?
+          responseObj.response : noCommandSet;
         // if the command is undefined re-assign it to an empty array
         // to avoid and error
         if (!commands){
@@ -37,21 +41,37 @@ module.exports = function(context){
         for( var index=0; index<commands.length; index++ ){
           // if the command matches,
           if ( message.match( commands[index] ) ){
+              // save this reseponse object
+              listObj = responseObj;
               // return it's response function
-              return ability.response;
+              return responseObj.response;
           }
         }
       }
       return notFound;
     };
 
-
+    // check for errors
+    tryContext(context);
     // Read
     var response = findResponse();
-    if( response ){
+    // if the response is a function
+    if( response && typeof response === "function"){
       // pass to the responder expecting
       // a new responseList in return
       return responder(response, context);
+    // Else check if the response is asking for a reader chain
+  } else if( response === 'read' ) {
+      // Check if the response has a new responseList
+      if( listObj.responseList ){
+        // replace the responseList that was just read
+        // with a responseList for the next reading
+        context.bot.responseList = listObj.responseList;
+        // Check that responseList for errors
+        tryContext(context);
+        // Recur by calling the reader and returning it's bubble up
+        return reader(context)
+      }//if
     } else {
       // if the response is undefined
       console.log("Reader Error: "+ ERROR_LOGS.RESPONSE_MAYBE_UNDEFINED );
@@ -59,3 +79,5 @@ module.exports = function(context){
       return 'endAbility';
     }
 }
+
+module.exports = reader;
